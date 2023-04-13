017ba392bb059573957b20ebcde4bd4c7956564d
Danya Pavlina <danyapavlina@gmail.com>
2023-04-13 09:46:24 +0200

    removed google auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type DefaultSession,
 } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -51,10 +50,6 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 };
